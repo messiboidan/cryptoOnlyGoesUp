cd9bf117ddf9a313c8329c21eff2812e74b9b143
messiboidan <dannyloes@gmail.com>
2021-04-28 08:06:44 -0500

    adding git ignore

--- bitcoinBot.py
+++ bitcoinBot.py
@@ -1,7 +1,7 @@
 from coinbase.wallet.client import Client
 from time import sleep
-from data import percentage_change, api_key, api_secret
-import mysql.connector
+from data import api_key, api_secret
+
 
 
 
@@ -11,35 +11,64 @@ import mysql.connector
 client = Client(api_key, api_secret)
 #payment_method = client.get_payment_methods()[0] //USED TO GRAB PAYMENT ID TO ACTUALLY MAKE THE PURCHASE
 
+
 #Take user input
 user_limit_order = float(input("Enter a price for your Bitcoin limit order (USD): "))
 user_amount_spent = float(input("Enter how much you want to spend (USD): "))
 
-#Creating the loop
-currency_code = 'USD'
 
-start_price = client.get_spot_price(currency=currency_code)
+#Creating the loop
+
+
+
+currencypair = 'ETH-USD'
+
+start_price = client.get_spot_price(currency_pair=currencypair)
+
+
+
+#method to calculate the % change in price. Returns float.
+#(d2 - d1) / d1
+def percentage_change( begin_price, end_price):
+    end = float(end_price)
+    return (end - (float(begin_price)))/(float(begin_price))
+
+
 
 while(True):
 
+
+
     #Reset currents and find percentage change
-    buy_price = client.get_buy_price(currency=currency_code)
+
+    buy_price = client.get_buy_price(currency_pair=currencypair)
+
     percentage_gainloss = percentage_change(start_price.amount, buy_price.amount)
 
+
     #print bitcoin curent price, and percentage chage
+
     print('Bitcoin is ' + str(buy_price.amount) + '\nPercent change in last 60 seconds: ' + format(percentage_gainloss, ".3f") + '%')
 
+
     #Within Purchase Threshold
+
     if(float(buy_price.amount) <= user_limit_order):
+
 
  #      buy = client.buy(amount=str(user_amount_spent / float(buy_price.amount), currency=currency_code, payment_method=payment_method.id))
 
+
         print("Bought $" + str(user_amount_spent) + " or " + str(user_amount_spent / float(buy_price.amount)) + " bitcoin at " + buy_price.amount)
+
 
     sleep(60)
 
+
     #Update start_price
+
     start_price = buy_price
+
 
 
 
---
+++ data.py
@@ -0,0 +1,2 @@
+api_key = 'FB9jZ0luhlvt0Sh7'
+api_secret = 'XNCEW44ij8Yax0OpwRqMm2tNIMv20jXt'
